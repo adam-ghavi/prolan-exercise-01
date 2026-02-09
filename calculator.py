bd8de36dd89cab39b0bb6a8d23d2fb5bd709c6c5
adam-ghavi <adamghaviyasha@gmail.com>
2026-02-09 16:42:44 +0700

    ganti nama variabel

--- calculator.py
+++ calculator.py
@@ -1,16 +1,16 @@
 class Calculator:
-    def add(self, a, b):
-        return a + b
-    def subtract(self, a, b):
-        return a - b
-    def multiply(self, a, b):
-        return a * b
-    def divide(self, a, b):
-        return a / b
-    def modulo(self, a, b):
-        return a % b
-    def power(self, a, b):
-        return a ** b
+    def add(self, t, h):
+        return t + h
+    def subtract(self, t, h):
+        return t - h
+    def multiply(self, t, h):
+        return t * h
+    def divide(self, t, h):
+        return t / h
+    def modulo(self, t, h):
+        return t % h
+    def power(self, t, h):
+        return t ** h
 
 if __name__ == "__main__":
     calc = Calculator()
